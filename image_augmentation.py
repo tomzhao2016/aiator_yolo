154b0a732e11f7488e64738ea78ea521455e2a48
QingyangZhao <276676925@qq.com>
2018-11-18 02:02:02 +0800

    add annotation path

---
+++ image_augmentation.py
@@ -0,0 +1,6 @@
+
+# this file is to save augmented data
+train_path = '/home/qingyang/aiator/data/location_images/train'
+val_path = '/home/qingyang/aiator/data/location_images/val'
+
+# augmentation including rotation (90 degree) translation
