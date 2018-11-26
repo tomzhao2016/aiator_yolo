3fbd473a55bee15f3d1230853a5f977b1efd10e0
QingyangZhao <276676925@qq.com>
2018-11-26 11:34:20 +0800

    smaller bb

--- create_annotations.py
+++ create_annotations.py
@@ -163,7 +163,7 @@ for i in range(len(lines_val)):
         ellipse_x_true = int((new_box[0] + new_box[2]) / 2)
         ellipse_y_true = int((new_box[1] + new_box[3]) / 2)
 
-        draw.rectangle([(ellipse_x_true - 50, ellipse_y_true - 10), (ellipse_x_true + 50, ellipse_y_true + 10)],
+        draw.rectangle([(ellipse_x_true - 30, ellipse_y_true - 150), (ellipse_x_true + 30, ellipse_y_true + 150)],
                            fill=(255))
         del draw
 
